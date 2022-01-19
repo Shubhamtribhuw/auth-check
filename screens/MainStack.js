70cf83b451ea55260fb3f985c939ef383dbbf16b
Shubham Y Tribhuwan <GSC-30868@GSLAB.COM>
2022-01-19 16:42:32 +0530

    update

--- screens/MainStack.js
+++ screens/MainStack.js
@@ -1,65 +1,95 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import Icon from "react-native-vector-icons/Ionicons";
+import { icons, COLORS } from "../constants";
 
-import Home from "./HomeScreen";
+import Home from "./Home";
 import SearchScreen from "./SearchScreen";
 import ProfileScreen from "./ProfileScreen";
+import BookDetail from "./BookDetail";
 
 const Tab = createMaterialBottomTabNavigator();
 
+const tabOptions = {
+  showLabel: false,
+  style: {
+    height: "10%",
+    backgroundColor: COLORS.black,
+  },
+};
+
 const MainStack = () => {
   return (
-    <Tab.Navigator initialRouteName="Home" activeColor="#fff">
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: "Home",
-          tabBarColor: "#009387",
-          tabBarIcon: ({ color }) => (
-            <Icon name="home" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="search"
-        component={SearchScreen}
-        options={{
-          tabBarLabel: "search",
-          tabBarColor: "#1f65ff",
-          tabBarIcon: ({ color }) => (
-            <Icon name="search" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarLabel: "Profile",
-          tabBarColor: "#694fad",
-          tabBarIcon: ({ color }) => (
-            <Icon name="person" color={color} size={26} />
-          ),
-        }}
-      />
+    <Tab.Navigator
+      tabBarOptions={tabOptions}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          const tintColor = focused ? COLORS.white : COLORS.gray;
+
+          switch (route.name) {
+            case "Home":
+              return (
+                <Image
+                  source={icons.dashboard_icon}
+                  resizeMode="contain"
+                  style={{
+                    tintColor: tintColor,
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+              );
+
+            case "SearchScreen":
+              return (
+                <Image
+                  source={icons.search_icon}
+                  resizeMode="contain"
+                  style={{
+                    tintColor: tintColor,
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+              );
+
+            case "BookDetail":
+              return (
+                <Image
+                  source={icons.notification_icon}
+                  resizeMode="contain"
+                  style={{
+                    tintColor: tintColor,
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+              );
+
+            case "ProfileScreen":
+              return (
+                <Image
+                  source={icons.plus_icon}
+                  resizeMode="contain"
+                  style={{
+                    tintColor: tintColor,
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+              );
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="SearchScreen" component={SearchScreen} />
+      <Tab.Screen name="BookDetail" component={BookDetail} />
+      <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
     </Tab.Navigator>
   );
 };
 
-const styles = StyleSheet.create({
-  shadow: {
-    shadowColor: "black",
-    shadowOpacity: 10,
-    shadowRadius: 20,
-    elevation: 25,
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-  },
-});
 export default MainStack;
